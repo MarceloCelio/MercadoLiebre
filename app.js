c98adb1c91507ad89882aa1adbc37e4f1e167c30
MarceloCelio <marcelocelio@gmail.com>
2022-12-15 22:56:42 -0300

    se sube el proyecto inicial

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const path = require ('path');
+const app = express();
+
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.set(('views', path.resolve(__dirname, './views')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+})
+app.get('/registro', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/registro.html'));
+})
+app.post('/registro', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/registro.html'));
+})
+
+
+
+app.listen (3001, () => {
+    console.log('Servidor corrindo en el puerto 3001');
+});
+
